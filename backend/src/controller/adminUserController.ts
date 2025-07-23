948a7fec558204b3e9d3fc117336f207a0d04d2e
asna sharin <asnasherin1233@gmail.com>
2025-07-23 22:49:29 +0530

    add get all student functionality in admin controller

--- backend/src/controller/adminUserController.ts
+++ backend/src/controller/adminUserController.ts
@@ -1,9 +1,117 @@
 import { NextFunction, Request, Response } from "express";
 import expressAsyncHandler from "express-async-handler";
+import Teacher from "../model/teacherProfile";
+import Student from "../model/studentProfile";
 
 
 // get all tutors
 export const getAllTutors = expressAsyncHandler(async(req: Request, res: Response, next: NextFunction) => {
     const query: {name?: { $regex: RegExp } } = {}
     const page: number = parseInt(req.query.page as string, 10) || 1;
+    const pageSize = 5
+    if (req.query.search) 
+        [(query.name = { $regex: new RegExp(req.query.search as string, "i") })];
+    const tachers = await Teacher.aggregate([
+      {
+        $match: query,
+      },
+      {
+        $lookup: {
+          from: "users",
+          localField: "userID",
+          foreignField: "_id",
+          as: "user",
+        },
+      },
+      {
+        $unwind: "$user",
+      },
+      {
+        $project: {
+          "user._id": 0,
+          "user.role": 0,
+          "user.password": 0,
+          __v: 0,
+          "user.__v": 0,
+          profile: 0,
+          bio: 0,
+          pricing: 0,
+        },
+      },
+      {
+        $skip: (page - 1) * pageSize,
+      },
+      {
+        $limit: pageSize,
+      },
+    ]);
+    let count = await Teacher.countDocuments();
+    count = ~~(count / 5);
+
+    if (tachers) {
+      res.status(200).json({
+        success: true,
+        teachers: tachers,
+        count: count,
+      });
+    } else {
+      next(Error("No teachers Registered"));
+    }
 })
+
+export const getAllstudnets = expressAsyncHandler(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const query: { name?: { $regex: RegExp } } = {};
+    const page: number = parseInt(req.query.page as string, 10) || 1;
+    const pageSize = 5;
+    if (req.query.search)
+      [(query.name = { $regex: new RegExp(req.query.search as string, "i") })];
+
+    const students = await Student.aggregate([
+      {
+        $match: query,
+      },
+      {
+        $lookup: {
+          from: "users",
+          localField: "userID",
+          foreignField: "_id",
+          as: "user",
+        },
+      },
+      {
+        $unwind: "$user",
+      },
+      {
+        $project: {
+          "user._id": 0,
+          "user.role": 0,
+          "user.password": 0,
+          __v: 0,
+          "user.__v": 0,
+          profile: 0,
+          gender: 0,
+          preffered_language: 0,
+          dob: 0,
+        },
+      },
+      {
+        $skip: (page - 1) * pageSize,
+      },
+      {
+        $limit: pageSize,
+      },
+    ]);
+    let count = await Student.countDocuments();
+    count = ~~(count / 5);
+    if (students) {
+      res.status(200).json({
+        success: true,
+        students: students,
+        count: count,
+      });
+    } else {
+      next(Error("No teachers Registered"));
+    }
+  }
+);
